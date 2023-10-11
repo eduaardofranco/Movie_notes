2aba44cb0698b5a58a886da36be048534933d996
eduaardofranco <eduaardo.franco@gmail.com>
2023-10-11 07:07:54 +0100

    user validated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,12 +3,14 @@ const multer = require('multer')
 const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../controllers/UsersController')
+const UsersValidatedController = require('../controllers/UsersValidatedController')
 const UserAvatarController = require('../controllers/UserAvatarController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const usersValidatedController = new UsersValidatedController()
 const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig.MULTER)
@@ -17,5 +19,6 @@ usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
 usersRoutes.delete('/:id', usersController.delete)
 usersRoutes.patch("/avatar", ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
+usersRoutes.get('/validated', ensureAuthenticated, usersValidatedController.index)
 
 module.exports = usersRoutes;
